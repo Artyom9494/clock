a50c14f404146ed54da30d030b3d9660c746df3e
Artyom Kalinkin <artyom.artyom.9494@mail.ru>
2022-02-04 15:19:11 +0300

    clock

---
+++ script.js
@@ -0,0 +1,20 @@
+const deg = 6;
+const hr = document.querySelector('.hr');
+const mi = document.querySelector('.mi');
+const sc = document.querySelector('.sc');
+
+
+
+
+setInterval(time, 1000)
+function time() {
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+
+
+    hr.style.transform = `rotateZ(${(hh) + (mm / 12)}deg)`;
+    mi.style.transform = `rotateZ(${(mm)}deg)`;
+    sc.style.transform = `rotateZ(${(ss)}deg)`;
+}
